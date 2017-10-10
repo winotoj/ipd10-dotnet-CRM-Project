6c523018cd37dd7d70a2a04634be8e4e38a75e57
STUDENTS\1795693 <v.seyedzadeh.985@gmail.com>
2017-10-10 13:29:59 -0400

    **add print to pdf and change code to ask the file path from user in export to excel

--- WpfCRMProject/WpfCRMProject/Report1.xaml.cs
+++ WpfCRMProject/WpfCRMProject/Report1.xaml.cs
@@ -14,6 +14,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using WpfCRMProject.Services;
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+using System.Data;
+using System.Reflection;
+using Microsoft.Win32;
 
 namespace WpfCRMProject
 {
@@ -59,13 +66,108 @@ namespace WpfCRMProject
                 lvAddress.Items.Add(c);
 
             }
-            var reducedList = customerList.Select(c => new { c.CompanyName, c.SalesRepId }).ToList();
+            var reducedList = customerList.Select(c => new { CompanyName = c.CompanyName , NumberOfBuys = c.SalesRepId }).ToList();
             
 
             var datatable = CreateExcelFile.ListToDataTable(reducedList);
-            CreateExcelFile.CreateExcelDocument(datatable, "ListOfPurchesOfCustomers.xls");
-            MessageBox.Show("All Information exported!", "Successfully", MessageBoxButton.OK, MessageBoxImage.Information);
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Excel file (*.xls)|*.xlsx";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                CreateExcelFile.CreateExcelDocument(datatable, saveFileDialog.FileName);
+                MessageBox.Show("All Information exported!", "Successfully", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
         }
 
+        private void btnPrinttoPDF_Click(object sender, RoutedEventArgs e)
+        {
+            List<Customer> customerList = rp.DisplayListOfPurchesOfCustomers();
+            lvAddress.Items.Clear();
+            foreach (Customer c in customerList)
+            {
+                lvAddress.Items.Add(c);
+
+            }
+            var reducedList = customerList.Select(c => new { CompanyName = c.CompanyName, NumberOfBuys = c.SalesRepId }).ToList();
+
+            DataTable dataTable = new DataTable(typeof(Customer).Name);
+            var datatable = ToDataTable(reducedList);
+            ExportToPdf(datatable);            
+        }
+
+        public static DataTable ToDataTable<T>(List<T> items)
+        {
+            DataTable dataTable = new DataTable(typeof(T).Name);
+
+            //Get all the properties
+            PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (PropertyInfo prop in Props)
+            {
+                //Defining type of data column gives proper data table 
+                var type = (prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>) ? Nullable.GetUnderlyingType(prop.PropertyType) : prop.PropertyType);
+                //Setting column names as Property names
+                dataTable.Columns.Add(prop.Name, type);
+            }
+            foreach (T item in items)
+            {
+                var values = new object[Props.Length];
+                for (int i = 0; i < Props.Length; i++)
+                {
+                    //inserting property values to datatable rows
+                    values[i] = Props[i].GetValue(item, null);
+                }
+                dataTable.Rows.Add(values);
+            }
+            //put a breakpoint here and check datatable
+            return dataTable;
+        }
+
+
+
+        public void ExportToPdf(DataTable dt)
+        {
+            Document document = new Document();
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PDF file (*.pdf)|*.pdf";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(saveFileDialog.FileName, FileMode.Create));
+                
+                document.Open();
+
+                iTextSharp.text.Font font5 = iTextSharp.text.FontFactory.GetFont(FontFactory.HELVETICA, 5);
+
+                PdfPTable table = new PdfPTable(dt.Columns.Count);
+
+                float[] widths = new float[dt.Columns.Count];
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    widths[i] = 4f;
+                }
+
+                table.SetWidths(widths);
+
+                table.WidthPercentage = 100;
+
+
+                foreach (DataColumn c in dt.Columns)
+                {
+                    table.AddCell(new Phrase(c.ColumnName, font5));
+                }
+
+                foreach (DataRow r in dt.Rows)
+                {
+                    if (dt.Rows.Count > 0)
+                    {
+                        for (int i = 0; i < dt.Columns.Count; i++)
+                        {
+                            table.AddCell(new Phrase(r[i].ToString(), font5));
+                        }
+                    }
+                }
+                document.Add(table);
+                document.Close();
+            }
+        }
     }
 }

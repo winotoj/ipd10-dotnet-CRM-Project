4b057d6ac2634d436408744a5d4855534b26b2f6
STUDENTS\1795661 <minx1208@hotmail.com>
2017-10-12 15:07:27 -0400

    * for sql query

--- script for sql/sales per user.sql
+++ script for sql/sales per user.sql
@@ -1,12 +1,18 @@
 SELECT u.username, sum(s.amount)
 FROM (Salesreps AS u INNER JOIN Customers AS c ON u.username = c.salesrep_id) INNER JOIN Sales AS s ON c.customer_id = s.customer_id WHERE YEAR(S.purchase_date) = YEAR(GETDATE())-1 GROUP BY u.username;
 
-SELECT u.username, sum(s.amount)
-FROM (Salesreps AS u INNER JOIN Customers AS c ON u.username = c.salesrep_id) INNER JOIN Sales AS s ON c.customer_id = s.customer_id WHERE MONTH(S.purchase_date) = MONTH(DATEADD(DD, -1, GETDATE())) GROUP BY u.username;
+SELECT u.username, MONTH(s.purchase_date) as mth, s.amount
+FROM (Salesreps AS u INNER JOIN Customers AS c ON u.username = c.salesrep_id) INNER JOIN Sales AS s ON c.customer_id = s.customer_id WHERE YEAR(S.purchase_date) = YEAR(DATEADD(DD, -1, GETDATE()));
 ;
 GO
 
-SELECT u.username, s.amount, s.purchase_date
-FROM (Salesreps AS u INNER JOIN Customers AS c ON u.username = c.salesrep_id) INNER JOIN Sales AS s ON c.customer_id = s.customer_id WHERE MONTH(S.purchase_date) = MONTH(DATEADD(DD, -1, GETDATE()))-1 order BY u.username;
+SELECT u.username, MONTH(s.purchase_date) as mth, sum(s.amount) as amt
+FROM (Salesreps AS u INNER JOIN Customers AS c ON u.username = c.salesrep_id) INNER JOIN Sales AS s ON c.customer_id = s.customer_id 
+WHERE YEAR(S.purchase_date) = YEAR(DATEADD(DD, -1, GETDATE())) group by MONTH(s.purchase_date), u.username 
+order by MONTH(s.purchase_date)
 ;
 GO
+
+
+
+
